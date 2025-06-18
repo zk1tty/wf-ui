870c6ab267e50af4fbe1afe5c45a47a77558a5c8
zk1tty <norikazkitty@gmail.com>
2025-06-18 22:15:45 +0800

    add vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import WorkflowLoader from './pages/WorkflowLoader';
 import ProcessingPage from './pages/ProcessingPage.tsx';
 import NotFound from './pages/NotFound';
 import { useExtensionIntegration } from './hooks/useExtensionIntegration';
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
